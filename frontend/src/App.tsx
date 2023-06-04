02e6a31ea659de9d2d1fc04661ea7f7ee43e3cc1
NeoElElegid <neelelegido@gmail.com>
2023-06-04 15:33:25 -0500

    init project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold text-blue-500">
+        Veterinaria
+      </h1>
+    </div>
+  )
+}
+
+export default App
