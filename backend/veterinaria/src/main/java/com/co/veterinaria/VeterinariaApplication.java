01f0d816340d30e4f3634ee194f9149ad4c0ea96
ElAgenteSmith <mimoro1175@hotmail.com>
2023-06-05 19:28:46 -0500

    Basic config for local database, gradle dependencies and remove spring default form security.

--- backend/veterinaria/src/main/java/com/co/veterinaria/VeterinariaApplication.java
+++ backend/veterinaria/src/main/java/com/co/veterinaria/VeterinariaApplication.java
@@ -2,12 +2,13 @@ package com.co.veterinaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class VeterinariaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VeterinariaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VeterinariaApplication.class, args);
+    }
 
 }
