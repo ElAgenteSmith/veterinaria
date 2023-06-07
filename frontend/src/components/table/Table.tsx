3b51ae60b008d73a641534ad24b66a2d28b978c4
sebastian.gomez <sebastiangc.2309@gmail.com>
2023-06-06 22:51:24 -0500

    add delete icon

--- frontend/src/components/table/Table.tsx
+++ frontend/src/components/table/Table.tsx
@@ -3,6 +3,7 @@ import { table } from './table.const'
 import { TableProps } from './table.types'
 import { getColumnValues } from 'utils/columnValues'
 import { useNavigate, useLocation } from 'react-router-dom'
+import { MdDeleteOutline } from 'react-icons/md'
 
 const Table: React.FC<TableProps> = ({ records, type, onDelete }) => {
   const navigate = useNavigate()
@@ -37,20 +38,7 @@ const Table: React.FC<TableProps> = ({ records, type, onDelete }) => {
               </td>
             ))}
             <td className="py-2 px-4 text-center">
-              <button onClick={() => onDelete(record?.id)}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="w-4 h-4 text-red-600 hover:text-red-700"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 2a8 8 0 015.74 13.41l3.61 3.61-1.42 1.42-3.61-3.61A8 8 0 112 10zm4 6a1 1 0 11-2 0V8a1 1 0 112 0v6zm-4 0a1 1 0 11-2 0V8a1 1 0 112 0v6z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
+              <MdDeleteOutline onClick={() => onDelete(record.id)} />
             </td>
           </tr>
         ))}
--- frontend/src/pages/users/UserDetail.tsx
+++ frontend/src/pages/users/UserDetail.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const UserDetail = () => {
-  return <div>UserDetail</div>
+  const { id } = useParams()
+  //call redux store to get user detail
+  return (
+    <div className="flex flex-col justify-center items-center gap-2 bg-grey-200 ">
+      <h2 className="font-bold mb-4">User with id: {id}</h2>
+      <div className="mt-10">
+        <p>
+          <strong>Name:</strong>
+        </p>
+        <p>
+          <strong>Identification:</strong>
+        </p>
+        <p>
+          <strong>Date of admission:</strong>
+        </p>
+        <p>
+          <strong>Direction:</strong>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default UserDetail
